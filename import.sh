50eb2b785c888011265452f044560b7a2e0adcd4
Anas Nashif <anas.nashif@intel.com>
2021-03-15 12:02:11 +0000

    import 1 commit
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- import.sh
+++ import.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-RESULTS_PATH=/home/nashif/testing/test_results/results/
-ZEPHYR_PATH=/var/www/tudiya.site/tools/repos/zephyr/
+RESULTS_REPO_PATH=/home/nashif/testing/test_results
+RESULTS_DIR=$RESULTS_DIR
+ZEPHYR_REPO_PATH=/var/www/tudiya.site/tools/repos/zephyr/
 INFLUX_DB=influxdb://localhost:8086/zephyr_test_results
 RUN=$1
 
 export PYTHONPATH=$PWD/junit2influx-0.2.1:$PYTHONPATH
 
 git -C $ZEPHYR_PATH pull --rebase
-git -C $RESULTS_PATH pull --rebase
+git -C $RESULTS_REPO_PATH pull --rebase
 
 if [ -z "$RUN" ]; then
-	for f in `ls -1 $RESULTS_PATH`; do
-		if [[ $f == *"v2.4.0"* ]]; then
-			for ff in `ls -1 $RESULTS_PATH/$f`; do
+	for f in `ls -1 $RESULTS_DIR`; do
+		if [[ $f == *"v2.5.0"* ]]; then
+			for ff in `ls -1 $RESULTS_DIR/$f`; do
 				platform=$(basename $ff .xml)
 				echo "$f ($platform)"
 				d=$(git -C $ZEPHYR_PATH log --format=%ct --date=local $f^..$f)
 				echo $d
-				junit2influx $RESULTS_PATH/$f/$ff --timestamp "$d" --tag platform=$platform --tag version=$f --influxdb-url $INFLUX_DB
+				junit2influx $RESULTS_DIR/$f/$ff --timestamp "$d" --tag platform=$platform --tag version=$f --influxdb-url $INFLUX_DB
 				sleep 2
 			done
 		 fi;
 	done
-else
-	for p in `ls -1 $RESULTS_PATH/$RUN`; do
+elif [ -d $RESULTS_DIR/$RUN ]; then
+	for p in `ls -1 $RESULTS_DIR/$RUN`; do
 		platform=$(basename $p .xml)
 		echo "$RUN ($platform)"
 		d=$(git -C $ZEPHYR_PATH log --format=%ct --date=local $RUN^..$RUN)
 		echo $d
-		junit2influx $RESULTS_PATH/$RUN/$p --timestamp "$d" --tag platform=$platform --tag version=$RUN --influxdb-url $INFLUX_DB
+		junit2influx $RESULTS_DIR/$RUN/$p --timestamp "$d" --tag platform=$platform --tag version=$RUN --influxdb-url $INFLUX_DB
 		sleep 2
 	done
 
+else
+	files=$(git -C $RESULTS_REPO_PATH show --pretty="" --name-only $RUN)
+	for f in `printf '%s\n' $files`; do
+		platform=$(basename $f .xml)
+		version_=$(dirname $f)
+		version=$(basename $version_)
+		d=$(git -C $ZEPHYR_PATH log --format=%ct --date=local $version^..$version)
+		junit2influx $RESULTS_REPO_PATH/$f --timestamp "$d" --tag platform=$platform --tag version=$version --influxdb-url $INFLUX_DB
+		sleep 2
+	done
 fi
